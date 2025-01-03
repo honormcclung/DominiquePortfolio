0f6bef1bc60cd806db31a1c1f0200a9de0f349e8
honormcclung <honor.mae.mcclung@gmail.com>
2025-01-03 03:59:14 -0600

    Centered navbar

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
+const navLeft = document.querySelector('.nav-links.nav-left');
+const navRight = document.querySelector('.nav-links.nav-right');
 
 menuToggle.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+  navLeft.classList.toggle('active');
+  navRight.classList.toggle('active');
 });
